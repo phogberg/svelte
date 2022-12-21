423c575ec97df529681dbee08c7052d3ed82eafa
Patrik Högberg <patriktroopr@gmail.com>
2022-12-21 22:54:58 +0100

    Initial

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-# Svelte Example
+# Builder.io example with Svelte + Vite
 
-This directory is a brief example of a [Svelte](https://svelte.dev/) app with [Serverless Functions](https://vercel.com/docs/v2/serverless-functions/introduction) that can be deployed with Vercel and zero configuration.
+This example shows our [Svelte SDK](/packages/sdks/output/svelte) with Svelte + Vite.
 
-## Deploy Your Own
+## Builder.io Setup
 
-Deploy your own Svelte project, along with Serverless Functions, with Vercel.
+- log into builder.io
+- from your account page, copy your API key and paste it into BUILDER_API_KEY in `App.svelte`
+- open the Builder.io Visual Editor for the model named "page"
+- enter http://localhost:3000 in the URL bar to the top right of the preview in Builder
+- drag a component into the layers tab, and it will appear in the Editor!
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/vercel/vercel/tree/main/examples/svelte)
+Checkout this Loom for a visual walkthrough: https://www.loom.com/share/afd7c9a1f8f148959ea0396be42560fd (it's originally intended for React-Native, but all of the steps are still the exact same)
 
-_Live Example: https://svelte.now-examples.now.sh_
+## Status
 
-### How We Created This Example
+For the status of the SDK, look at [these tables](/packages/sdks/README.md#feature-implementation).
 
-To get started with Svelte, along with [Serverless Functions](https://vercel.com/docs/v2/serverless-functions/introduction), deployed with Vercel, you can use [degit](https://github.com/Rich-Harris/degit) to initialize the project:
+## Build Setup
 
-```shell
-$ npx degit sveltejs/template my-svelte-project
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+# or start the server and open the app in a new browser tab
+$ npm run dev -- --open
 ```
--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,43 @@
 <script>
-  import { onMount } from "svelte";
-  export let date;
+  import { getContent, isPreviewing, RenderContent } from '@builder.io/sdk-svelte';
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
+  // Add your Public API Key
+  const BUILDER_PUBLIC_API_KEY = "af78a16b5e3245c0aa1e501ea73f4c6c"; 
+
+  let content = undefined;
+  let canShowContent = false;
+
+  const fetch = async () => {
+    // fetch your Builder content
+    content = await getContent({
+      model: 'page',
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      userAttributes: {
+        urlPath: window.location.pathname || '/'
+      }
+    });
+
+    // handle preview mode
+    canShowContent = Boolean(content || isPreviewing());
+  };
+
+  fetch();
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/main/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <h1>
+    Welcome to your Builder-integrated Svelte app
+  </h1>
+  <div>Below is your Builder Content:</div>
+
+  {#if canShowContent}
+    <!-- Render builder content with all required props -->
+    <RenderContent
+      model="page"
+      {content}
+      apiKey={BUILDER_PUBLIC_API_KEY}
+    />
+  {:else}
+    Content Not Found
+  {/if}
 </main>
